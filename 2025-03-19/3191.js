d18239c30d8d1f17ec3e10dcd93acbc542eb1690
Sxnty <sxntydev@gmail.com>
2025-03-19 20:54:45 -0300

    Improve solution performance (-1ms, total 6ms)

--- 2025-03-19/3191.js
+++ 2025-03-19/3191.js
@@ -4,18 +4,17 @@
  */
 var minOperations = function (nums) {
   let operations = 0;
+
   for (let i = 0; i <= nums.length - 3; i++) {
     if (nums[i] === 0) {
-      if (nums[i + 1] !== undefined && nums[i + 2] !== undefined) {
-        nums[i] = 1;
-        nums[i + 1] = 1 - nums[i + 1];
-        nums[i + 2] = 1 - nums[i + 2];
-        operations++;
-      } else {
-        return -1;
-      }
+      nums[i] = 1;
+      nums[i + 1] = 1 - nums[i + 1];
+      nums[i + 2] = 1 - nums[i + 2];
+      operations++;
     }
   }
-  return nums.includes(0) ? -1 : operations;
+  return nums[nums.length - 1] === 1 && nums[nums.length - 2] === 1
+    ? operations
+    : -1;
 };
-console.log(minOperations([0,1,1,1,0,0])); // ✅ Debe dar 3
+console.log(minOperations([0, 1, 1, 1, 0, 0,0, 1, 1, 1, 0, 0]));
